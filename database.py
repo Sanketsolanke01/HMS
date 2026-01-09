0b969c4a7fe01e1edd6e2323ef834203d121d15c
Sanketsolanke01 <120560336+Sanketsolanke01@users.noreply.github.com>
2026-01-09 22:35:48 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from  sqlalchemy.orm import sessionmaker,declarative_base
 
-DATABASE_URL="postgresql://postgres:Sanket123@localhost:5432/hms"
+DATABASE_URL="postgresql://hms_6tnp_user:IlTgZZD89jLvcNShsrCMzNNEv2ADPQ7L@dpg-d5gj0e6r433s73dnbfbg-a.oregon-postgres.render.com/hms_6tnp"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
+
 Base=declarative_base()
