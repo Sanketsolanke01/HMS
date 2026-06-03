47ce3563f4db7c071950d4ecc411a1b9a3e7ac12
Sanket Solanke <120560336+Sanketsolanke01@users.noreply.github.com>
2026-06-03 11:39:16 +0530

    database update

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from  sqlalchemy.orm import sessionmaker,declarative_base
 
 # DATABASE_URL="postgresql://postgres:Sanket123@localhost:5432/hms"
-DATABASE_URL="postgresql://hms_3xku_user:rGl1fJBNiBcPKiP3UmCI2bOVGaxhIwuP@dpg-d67aubggjchc73ai9m0g-a.oregon-postgres.render.com/hms_3xku"
+DATABASE_URL="postgresql://hms_ihbr_user:OftZ6bH3SVKnGNNOdC1pG7y4PvVz1kji@dpg-d8fs8in40ujc73bd6a80-a.oregon-postgres.render.com/hms_ihbr"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal=sessionmaker(autoflush=False,autocommit=False,bind=engine)
